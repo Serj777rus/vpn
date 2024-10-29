022a10cdec525dbb298880f93125de07592f6f97
Sergey <gvsergey89@gmail.com>
2024-10-29 23:42:13 +0300

    nodemon install

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,225 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="main_block">
+    <div class="main_div">
+      <div class="left_block">
+        <img src="../src/assets/vpn_logo.png" alt="logo">
+      </div>
+      <div class="right_block">
+        <div class="description">
+          <p>Бесплатный VPN для предпринимателей</p>
+        </div>
+        <div class="utps">
+          <div class="utp">
+            <img src="../src/assets/Star.png" alt="star">
+            <p>Высокая скорость</p>
+          </div>
+          <div class="utp">
+            <img src="../src/assets/Star.png" alt="star">
+            <p>Безлимитный трафик</p>
+          </div>
+          <div class="utp">
+            <img src="../src/assets/Star.png" alt="star">
+            <p>Конфиденциальность</p>
+          </div>
+        </div>
+        <div class="form">
+          <p>Заполните форму и доступ с инструкцией придет Вам на почту</p>
+          <form @submit.prevent="sendData">
+            <input type="text" name="name" id="name" placeholder="Имя" />
+            <input type="email" name="email" id="email" placeholder="email">
+            <div class="check_box">
+              <input type="text" name="site" id="site" placeholder="Сайт вашего бизнеса" :disabled="isCheck" />
+              <input type="checkbox" name="check" id="check" v-model="isCheck" />
+              <p>У меня нет сайта</p>
+            </div>
+            <div class="check_box2">
+              <input type="checkbox" name="check2" id="check2" v-model="isCheckBtn" />
+              <p>Отправляя данную форму Вы соглашаетесь с политикой конфиденциальности и получением коммерческих предложений</p>
+            </div>
+            <button type="submit" :disabled="!isCheckBtn">Отправить</button>
+          </form>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isCheck: false,
+      isCheckBtn: false,
+    }
+  },
+  methods: {
+    async sendData() {
+
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.main_block {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eaeaea;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.main_div {
+  width: 1200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 32px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.left_block {
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+}
+.left_block img {
+  width: 100%;
+  object-fit: contain;
+}
+.right_block {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  box-sizing: border-box;
+}
+.description {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 24px;
+}
+.description p {
+  font-size: 48px;
+  font-weight: 900;
+  color: #2c3e50;
+  line-height: 100%;
+  margin-bottom: 0;
+}
+.form {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  box-sizing: border-box;
+  padding: 24px;
+  background: #2c3e50;
+  border-radius: 24px;
+}
+.form p {
+  font-size: 32px;
+  text-align: center;
+  line-height: 100%;
+  color: #fff;
+  font-weight: 600;
+}
+.form form {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 16px;
+}
+input {
+  box-sizing: border-box;
+  padding: 8px 12px;
+  border-radius: 8px;
+  background: transparent;
+  border: #eaeaea solid 1px;
+  color: #eaeaea;
+}
+input:focus {
+  outline: none;
+}
+input::placeholder {
+  color: #fff;
+}
+.check_box {
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+}
+.check_box input {
+  width: 100%;
+}
+.check_box input[type='checkbox'] {
+  width: fit-content;
+}
+.check_box p {
+  font-size: 12px;
+  text-align: start;
+  margin-bottom: 0;
+  font-weight: 600;
+  white-space: nowrap;
+}
+.check_box2 {
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+}
+.check_box2 p {
+  font-size: 12px;
+  text-align: start;
+  margin-bottom: 0;
+  font-weight: 300;
+}
+form button {
+  box-sizing: border-box;
+  padding: 8px 24px;
+  border: none;
+  border-radius: 8px;
+  background: blanchedalmond;
+  align-self: center;
+  box-shadow: 0 2px 8px rgba(255, 255, 255, 0);
+  transform: translateY(0);
+  transition: all 300ms ease-in-out;
+  color: #2c3e50;
+  font-weight: 500;
+  cursor: pointer;
+}
+form button:disabled {
+  box-shadow: none;
+  background: burlywood;
+}
+form button:disabled:hover {
+  box-shadow: none;
+  background: burlywood;
+  transform: translateY(0);
+}
+form button:hover {
+  box-shadow: 0 2px 8px rgba(255, 255, 255, 0.3);
+  transform: translateY(-4px);
+}
+.utps {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.utp {
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  align-items: center;
+}
+.utp p {
+  font-weight: 600;
 }
 </style>
